65d22918fd1cd2527906fdbc4568cf5a995b5309
xavier.dutoit <xavier@tttp.eu>
2020-05-10 11:33:44 +0200

    new hook on the block

---
+++ index.js
@@ -0,0 +1,26 @@
+//react-geolocate
+
+import { useState, useEffect } from "react";
+
+const useGeoLocation = () => {
+  const [country, setCountry] = useState(null);
+  const [errors, setErrors] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchAPI() {
+      setIsLoading(true);
+      const res= await fetch("https://api.country.is");
+
+      res.json()
+      .then(res => setCountry(res?.country))
+      .catch(err => setErrors(err))
+      .finally(() => setIsLoading(false));
+    }
+    fetchAPI();
+  },[]);
+
+  return {country,errors,isLoading}
+};
+
+export default useGeoLocation;
