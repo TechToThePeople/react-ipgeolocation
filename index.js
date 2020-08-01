c6ff44e0bfb7b08e2ac439bbcf81a2c6266dae42
xavier.dutoit <xavier@tttp.eu>
2020-08-01 10:12:56 +0200

    handle better if the component is unmounted while it queries the geoapi

--- index.js
+++ index.js
@@ -3,27 +3,38 @@
 import { useState, useEffect } from "react";
 
 const useGeoLocation = (options = {}) => {
-  const [country, setCountry] = useState(null);
+  const [country, setCountry] = useState(options.country);
   const [error, setError] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const api = options.api || "https://api.country.is";
 
   useEffect(() => {
+    let isCancelled = false;
+
+    if (country) return;
     async function fetchAPI() {
       setIsLoading(true);
       await fetch(api)
-      .then (res => {
-        if (!res.ok) { throw Error(res.statusText);return null; }
-        return res.json()
-      })
-      .then(res => {if (res && res.country) setCountry(res.country);})
-      .catch(err => setError(err))
-      .finally(() => setIsLoading(false));
+        .then(res => {
+          if (!res.ok) {
+            throw Error(res.statusText);
+            return null;
+          }
+          return res.json();
+        })
+        .then(res => {
+          if (res && res.country && !isCancelled) setCountry(res.country);
+        })
+        .catch(err => setError(err))
+        .finally(() => setIsLoading(false));
     }
     fetchAPI();
-  },[]);
+    return () => {
+      isCancelled = true;
+    };
+  }, []);
 
-  return {country,error,isLoading}
+  return { country, error, isLoading };
 };
 
 export default useGeoLocation;
